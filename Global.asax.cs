676200fbbd64df93aa0d4153a58bc6129820caf5
shoaib22dev <72797033+shoaib22dev@users.noreply.github.com>
2023-03-05 16:19:16 +0530

    Added Identity
    Login Model

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿
+using DataServices;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Unity;
+
 
 namespace bewitching
 {
@@ -12,10 +17,15 @@ namespace bewitching
     {
         protected void Application_Start()
         {
+            //var service = IServiceCollection
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // UnityConfig.RegisterComponents();
+           
+
+
         }
     }
 }
